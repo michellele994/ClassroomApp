5d9364a9b69c87da5d11f44df13b640802e14aea
Perla Ballesteros <perlaballesteros@email.arizona.edu>
2018-02-25 12:16:24 -0700

    seq still not working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-//var db = require("./models");
+var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 
@@ -35,8 +35,8 @@ app.use(routes);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-//db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-//});
+});
