cf913b8069587fbe4712f718054db8135d604a7b
Perla Ballesteros <perlaballesteros@email.arizona.edu>
2018-02-25 21:35:01 -0700

    added ajax request to check if user exists, incorporated michelle's ajax request to post a new user if it username is unique

--- public/js/login.js
+++ public/js/login.js
@@ -17,26 +17,55 @@ $(function() {
 		event.preventDefault();
 		const username = $("#enter_username").val().trim();
 		const name = $("#enter_name").val().trim();
+		var userExists=false;
+		var userNameexists=false;
+
 		var newUser = {
 			username: username,
 			name: name
 		}
-
+		//make sure that both required fields have been entered before creating an account
 		if (username && name)
 		{
-			$.ajax("/api/users", {
-				type: "POST",
-				data: newUser
-			}).then(
-			function() {
-				alert("congrats you have created an account.Enter your inforamtion again to log in");
-				location.reload();
+			//before sending post request we must make sure that users dont exist
+			$.ajax("api/users/",{
+				//type:"GET"
+		
+			}).then(function(response){
+				//console.log(response[1].name);
+				for(var i=0;i<response.length;i++){
+					if(response[i].name===name&&response[i].username===username){
+						userExists=true;
+					}
+					if(response[i].username===username){
+						userNameexists=true;
+					}
+				}
+				if(userExists===true){
+					alert("This user already exists!");
+				}
+				if(userNameexists===true){
+					alert("Username is taken");
+				}
+				if(userExists===false&&userNameexists===false){
+					//Create a new user
+					$.ajax("/api/users", {
+						type: "POST",
+						data: newUser
+					}).then(
+					function() {
+						alert("congrats you have created an account.Enter your inforamtion again to log in");
+						location.reload();
+					});
+				}
 			});
 		}
 		else
 		{
 			alert("please enter a username and password");
-		}
+		}	
+			
+			
 	});
 	
 
--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/users", function(req, res) {
+  app.get("/api/users", function(req, res ,cb) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
@@ -9,11 +9,29 @@ module.exports = function(app) {
       res.json(dbusers);
     });
   });
-
   app.post("/api/users", function(req, res) {
     db.userTable.create(req.body).then(function(dbusers) {
       res.json(dbusers);
     });
   });
+
+  /*//route to see if user exists
+  app.get("/api/users/:username/:name",function(req,res){
+    db.userTable.findAll({
+      where:{
+        name:req.params.name,
+        username:req.params.username
+      }
+    }).then(function(dbusers) {
+      res.json(dbusers);
+    });
+  });
+  app.post("/api/users/:username/:name", function(req, res) {
+    db.userTable.create(req.body).then(function(dbusers) {
+      res.json(dbusers);
+    });
+  });*/
+
+  
 
 };
