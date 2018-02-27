12b2ecd5d4898c9fabd5955ab664151481e3a1ff
Brandon Haines <haines.brandon.91@gmail.com>
2018-02-27 12:06:13 -0700

    minor adjustments

--- routes/routes.js
+++ routes/routes.js
@@ -49,7 +49,7 @@ router.get("/api/users/:username/",function(req,res){
     res.json(dbuser);
   });
 });
-//ading a route to the classes pag
+//adding a route to the classes page
 router.get("/classes/:username/",function(req,res){
   db.userTable.findOne({
     where:{
