17fa9ea2ad886205a7c99a93cc5a3d18ef539b87
Perla Ballesteros <perlaballesteros@email.arizona.edu>
2018-03-05 08:35:50 -0700

    am i up to date with the changes

--- models/existingclass.js
+++ models/existingclass.js
@@ -22,11 +22,7 @@ module.exports = function(sequelize, DataTypes) {
 		  }
 		});
 		ExistingClass.belongsToMany(models.Student, { through: "StudentClassroomRoster"});
-<<<<<<< HEAD
 		ExistingClass.hasMany(models.AssignedHW);
-=======
-		// ExistingClass.hasMany(models.AssignedHW);
->>>>>>> f322351f99fe84b402a5687098204750cb982cc5
   	};
 	return ExistingClass;
 }
